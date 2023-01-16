b0f20959d1faa76dd866d0bdec4a4317fc5b95a3
Fernando Sproviero <fernandospr@gmail.com>
2023-01-16 19:29:12 -0300

    Externalizing dependencies

---
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -0,0 +1,32 @@
+object Deps {
+    const val coreKtx = "androidx.core:core-ktx:1.7.0"
+    const val appCompat = "androidx.appcompat:appcompat:1.5.1"
+    const val material = "com.google.android.material:material:1.7.0"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.1.4"
+    const val swiperefreshLayout = "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0"
+    const val palette = "androidx.palette:palette:1.0.0"
+
+    private const val glideVersion = "4.14.2"
+    const val glide = "com.github.bumptech.glide:glide:$glideVersion"
+    const val glideCompiler = "com.github.bumptech.glide:compiler:$glideVersion"
+
+    private const val retrofitVersion = "2.9.0"
+    const val retrofit = "com.squareup.retrofit2:retrofit:$retrofitVersion"
+    const val retrofitConverterGson = "com.squareup.retrofit2:converter-gson:$retrofitVersion"
+    const val retrofitAdapterRxJava = "com.squareup.retrofit2:adapter-rxjava2:$retrofitVersion"
+
+    const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:4.10.0"
+
+    const val rxAndroid = "io.reactivex.rxjava2:rxandroid:2.0.1"
+
+    const val koinAndroid = "io.insert-koin:koin-android:3.3.1"
+
+    private const val roomVersion = "2.4.3"
+    const val roomRuntime = "androidx.room:room-runtime:$roomVersion"
+    const val roomCompiler = "androidx.room:room-compiler:$roomVersion"
+    const val roomRxJava = "androidx.room:room-rxjava2:$roomVersion"
+
+    const val jUnit = "junit:junit:4.13.2"
+    const val mockitoKotlin = "org.mockito.kotlin:mockito-kotlin:4.1.0"
+    const val coreTesting = "androidx.arch.core:core-testing:2.1.0"
+}
